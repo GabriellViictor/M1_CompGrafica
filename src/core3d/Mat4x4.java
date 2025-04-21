1db779fca43bc7183bc619adbedd82046b4244ce
Gabriel Victor <63075215+GabriellViictor@users.noreply.github.com>
2025-04-21 15:23:56 -0300

    -

---
+++ src/core3d/Mat4x4.java
@@ -0,0 +1,205 @@
+package core3d;
+
+public class Mat4x4 {
+	float mat[][] = new float[4][4];
+	
+	public Mat4x4() {
+		setIdentity();
+	}
+	public void setIdentity() {
+		zera();
+		mat[0][0] = 1;
+		mat[1][1] = 1;
+		mat[2][2] = 1;
+		mat[3][3] = 1;
+	}
+	
+	public void zera() {
+		for(int y = 0; y < 4;y++) {
+			for(int x = 0; x < 4;x++) {
+				mat[y][x] = 0;
+			}
+		}
+	}
+	
+	public void setTranslate(float a,float b,float c) {
+		zera();
+		mat[0][0] = 1;
+		mat[1][1] = 1;
+		mat[2][2] = 1;
+		
+		mat[0][3] = a;
+		mat[1][3] = b;
+		mat[2][3] = c;
+		mat[3][3] = 1;
+	}
+	
+	public void setSacale(float a,float b,float c) {
+		zera();
+		mat[0][0] = a;
+		mat[1][1] = b;
+		mat[2][2] = c;
+		mat[3][3] = 1;
+	}
+	
+	public void setRotateY(float ang) {
+		zera();
+		float rad = ang*0.017453f;
+		float sin = (float)Math.sin(rad);
+		float cos = (float)Math.cos(rad);
+		mat[0][0] = cos;
+		mat[0][1] = 0;
+		mat[0][2] = -sin;
+		mat[0][3] = 0;
+		
+		mat[1][0] = 0;
+		mat[1][1] = 1;
+		mat[1][2] = 0;
+		mat[1][3] = 0;
+		
+		mat[2][0] = sin;
+		mat[2][1] = 0;
+		mat[2][2] = cos;
+		mat[2][3] = 0;
+		
+		mat[3][0] = 0;
+		mat[3][1] = 0;
+		mat[3][2] = 0;
+		mat[3][3] = 1;
+	}
+
+
+	public void setRotateX(float ang) {
+		zera();
+		float rad = ang*0.017453f;
+		float sin = (float)Math.sin(rad);
+		float cos = (float)Math.cos(rad);
+		mat[0][0] = 1;
+		mat[0][1] = 0;
+		mat[0][2] = 0;
+		mat[0][3] = 0;
+
+		mat[1][0] = 0;
+		mat[1][1] = cos;
+		mat[1][2] = -sin;
+		mat[1][3] = 0;
+
+		mat[2][0] = 0;
+		mat[2][1] = sin;
+		mat[2][2] = cos;
+		mat[2][3] = 0;
+
+		mat[3][0] = 0;
+		mat[3][1] = 0;
+		mat[3][2] = 0;
+		mat[3][3] = 1;
+	}
+
+
+	public void setRotateZ(float ang) {
+		zera();
+		float rad = ang*0.017453f;
+		float sin = (float)Math.sin(rad);
+		float cos = (float)Math.cos(rad);
+		mat[0][0] = cos;
+		mat[0][1] = -sin;
+		mat[0][2] = 0;
+		mat[0][3] = 0;
+
+		mat[1][0] = sin;
+		mat[1][1] = cos;
+		mat[1][2] = 0;
+		mat[1][3] = 0;
+
+		mat[2][0] = 0;
+		mat[2][1] = 0;
+		mat[2][2] = 1;
+		mat[2][3] = 0;
+
+		mat[3][0] = 0;
+		mat[3][1] = 0;
+		mat[3][2] = 0;
+		mat[3][3] = 1;
+	}
+
+	public void setRotateAny(float ang, float x, float y, float z) {
+		zera();
+
+		float length = (float)Math.sqrt(x*x + y*y + z*z);
+
+		if (length == 0) length = 1; // evita divisão por zero
+
+		float ux = x/length;
+		float uy = y/length;
+		float uz = z/length;
+
+		float rad = ang * 0.017453f;
+		float cos = (float)Math.cos(rad);
+		float sin = (float)Math.sin(rad);
+		float oneMinusCos = 1.0f - cos;
+
+		mat[0][0] = cos + ux*ux*oneMinusCos;
+		mat[0][1] = ux*uy*oneMinusCos - uz*sin;
+		mat[0][2] = ux*uz*oneMinusCos + uy*sin;
+		mat[0][3] = 0;
+
+		mat[1][0] = uy*ux*oneMinusCos + uz*sin;
+		mat[1][1] = cos + uy*uy*oneMinusCos;
+		mat[1][2] = uy*uz*oneMinusCos - ux*sin;
+		mat[1][3] = 0;
+
+		mat[2][0] = uz*ux*oneMinusCos - uy*sin;
+		mat[2][1] = uz*uy*oneMinusCos + ux*sin;
+		mat[2][2] = cos + uz*uz*oneMinusCos;
+		mat[2][3] = 0;
+
+		mat[3][0] = 0;
+		mat[3][1] = 0;
+		mat[3][2] = 0;
+		mat[3][3] = 1;
+	}
+
+	public void pointView(float ang, float d) {
+		zera();
+
+		mat[0][0] = 1;
+		mat[0][1] = 0;
+		mat[0][2] = 0;
+		mat[0][3] = 0;
+
+		mat[1][0] = 0;
+		mat[1][1] = 1;
+		mat[1][2] = 0;
+		mat[1][3] = 0;
+
+		mat[2][0] = 0;
+		mat[2][1] = 0;
+		mat[2][2] = 0;
+		mat[2][3] = 0;
+
+		mat[3][0] = 0;
+		mat[3][1] = 0;
+		mat[3][2] = 1/d;
+		mat[3][3] = 1;
+	}
+
+
+
+//	public void setRotateAxis(float ang,float x0,float y0) {
+//		zera();
+//		float rad = ang*0.017453f;
+//		float sin = (float)Math.sin(rad);
+//		float cos = (float)Math.cos(rad);
+//		mat[0][0] = cos;
+//		mat[0][1] = -sin;
+//		mat[0][2] = x0*(1-cos)+y0*sin;
+//		
+//		mat[1][0] = sin;
+//		mat[1][1] = cos;
+//		mat[1][2] = y0*(1-cos)-x0*sin;
+//		
+//		mat[2][0] = 0;
+//		mat[2][1] = 0;
+//		mat[2][2] = 1;
+//	}	
+}
---
+++ src/core3d/Triangulo3D.java
@@ -0,0 +1,90 @@
+package core3d;
+
+import java.awt.Graphics2D;
+
+public class Triangulo3D {
+	Ponto3D pa;
+	Ponto3D pb;
+	Ponto3D pc;
+
+	public Triangulo3D(Ponto3D a, Ponto3D b, Ponto3D c) {
+		super();
+		this.pa = new Ponto3D(a);
+		this.pb = new Ponto3D(b);
+		this.pc = new Ponto3D(c);
+	}
+
+	public Triangulo3D(Triangulo3D tri){
+		this.pa = new Ponto3D(tri.pa);
+		this.pb = new Ponto3D(tri.pb);
+		this.pc = new Ponto3D(tri.pc);
+	}
+	
+	public void desenhase(Graphics2D dbg) {
+		dbg.drawLine((int)pa.x,(int)pa.y,(int)pb.x,(int)pb.y);
+		dbg.drawLine((int)pb.x,(int)pb.y,(int)pc.x,(int)pc.y);
+		dbg.drawLine((int)pc.x,(int)pc.y,(int)pa.x,(int)pa.y);
+	}
+	
+	public void translacao(float a,float b, float c) {
+		Mat4x4 m = new Mat4x4();
+		m.setTranslate(a, b,c);
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}
+	public void escala(float a,float b,float c) {
+		Mat4x4 m = new Mat4x4();
+		m.setSacale(a, b, c);
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}	
+	
+	public void rotacaoY(float ang) {
+		Mat4x4 m = new Mat4x4();
+		m.setRotateY(ang);
+		//System.out.println("rot Y");
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}
+
+	public void rotacaoX(float ang) {
+		Mat4x4 m = new Mat4x4();
+		m.setRotateX(ang);
+		//System.out.println("rot Y");
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}
+
+	public void rotacaoZ(float ang) {
+		Mat4x4 m = new Mat4x4();
+		m.setRotateZ(ang);
+		//System.out.println("rot Y");
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}
+
+	public void anyRotation(float ang, float x, float  y, float z) {
+		Mat4x4 m = new Mat4x4();
+		m.setRotateAny(ang,x, y, z);
+		//System.out.println("rot Y");
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}
+
+	public void projecoes(float ang, float x) {
+		Mat4x4 m = new Mat4x4();
+		m.pointView(ang,x);
+		//System.out.println("rot Y");
+		pa.multiplicaMat(m);
+		pb.multiplicaMat(m);
+		pc.multiplicaMat(m);
+	}
+
+
+}
